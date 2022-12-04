efd63756cdad5bc24164e0dec7e169edfa59ed72
Filip Penzar <fpenzar@gmail.com>
2022-12-04 12:29:00 +0100

    Relative path fix

--- analizator/SA.py
+++ analizator/SA.py
@@ -1,10 +1,19 @@
 from constants import *
 from LR import LR
-from gen_tree import GenTree
+import os
+
+def get_analizator_dir():
+    # get the right path to the files
+    analizator_dir = os.path.abspath(os.getcwd())
+    if not analizator_dir.endswith('analizator') and not analizator_dir.endswith('analizator' + os.sep):
+        analizator_dir = os.path.join(analizator_dir, 'analizator')
+    return analizator_dir
 
 
 def main():
-    lr = LR(REDUCTIONS_FILE, LR_TABLE_FILE)
+    analizator_dir = get_analizator_dir()
+
+    lr = LR(os.path.join(analizator_dir, REDUCTIONS_FILE), os.path.join(analizator_dir, LR_TABLE_FILE))
     d = "d 1 d"
     c = "c 2 c"
     input = [c, c, d, c, d]
