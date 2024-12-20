b311d96218b2600fb197c14e27d0b8298b3890d6
pkukic <patrikkukic@gmail.com>
2024-12-20 16:15:45 +0100

    test i guess

---
+++ test_lab.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ROOT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ANALIZATOR_DIR="${ROOT_DIR}/analizator"
+
+for i in {1..13}
+do
+    rm "${ANALIZATOR_DIR}/reductions.txt" > /dev/null 2>&1
+    rm "${ANALIZATOR_DIR}/LR_table.txt" > /dev/null 2>&1
+    echo "#################################################################################################"
+    echo "Test $i"
+        # if [ "$i" == "8" ] || [ "$i" == "13" ]
+        # then
+        #     continue
+        # fi
+    python3 GSA.py < "$ROOT_DIR/testovi/test_$i/test.san"
+    res=$(python3 $ANALIZATOR_DIR/SA.py < $ROOT_DIR/testovi/test_$i/test.in | diff $ROOT_DIR/testovi/test_$i/test.out -)
+    if [ "$res" != "" ]
+    then
+        echo "FAIL"
+        echo $res
+    else
+        echo "OK"
+    fi
+    rm "${ANALIZATOR_DIR}/reductions.txt" > /dev/null 2>&1
+    rm "${ANALIZATOR_DIR}/LR_table.txt" > /dev/null 2>&1
+done
